72f03a45d2dcfe49ff84e264e2ff05027bcb69d5
Pidr <sshsrmlt@gmail.com>
2024-01-20 16:18:10 +0300

    All changes

--- src/com/pidr/Main.java
+++ src/com/pidr/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("ueban");
+        System.out.print("blyadina");
+        System.out.print("i feel really bad");
     }
 }
