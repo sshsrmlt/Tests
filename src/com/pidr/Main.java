c9682cadf75eb81c641df3adb81f3811d015e595
Developer <sshsrmlt@mail.ru>
2024-01-23 13:02:19 +0300

    Commit about my anxiety

--- src/com/pidr/Main.java
+++ src/com/pidr/Main.java
@@ -6,5 +6,7 @@ public class Main {
         System.out.println("ueban");
         System.out.print("blyadina");
         System.out.print("i feel really bad");
+        System.out.print("\n");
+        System.out.println("I feel terrible anxiety");
     }
 }
