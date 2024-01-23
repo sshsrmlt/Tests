fbba0c3dee65c193b36ea8e6e5e9858b5bd19462
Developer <sshsrmlt@mail.ru>
2024-01-23 13:28:33 +0300

    Some improvements

--- src/com/pidr/Main.java
+++ src/com/pidr/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("ueban");
-        System.out.print("blyadina");
-        System.out.print("i feel really bad");
+        System.out.println("blyadina");
+        System.out.println("i feel really bad");
     }
 }
